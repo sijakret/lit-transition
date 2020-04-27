7d305450d2e87f4778fd6d967129f2f66cb7a953
JanK <sijakret@gmail.com>
2020-04-27 02:58:54 +0200

    break bundle test

--- test/bundle/src.js
+++ test/bundle/src.js
@@ -1,5 +1,5 @@
 import {render,html} from 'lit-html';
-import {transition, mark} from 'lit-transition';
+import {transition, mark} from 'lit-transition1';
 
 // super basic sanity test
 render(transition(mark(html`<div></div>`,'test'), document.body));
