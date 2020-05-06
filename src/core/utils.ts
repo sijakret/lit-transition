f36239dfd5b17ebecee1d7804bc170fd6bee9aed
JanK <sijakret@gmail.com>
2020-05-06 19:21:21 +0200

    improve locking and nav

--- src/core/utils.ts
+++ src/core/utils.ts
@@ -56,41 +56,51 @@ export function marked(templateResult:TemplateResult) {
  */
 export function recordExtents(e:any) {
   const rect = e.getBoundingClientRect();
-  let top = 0;
-  let left = 0;
+  // // we separately track margins
+  // // so in case the changed when the extents
+  // // are reapplied (like when a fixed margin is used)
+  // // we can compensate
+  // const style = window.getComputedStyle(e);
+  // const marginTop = parseFloat(style.marginTop) || 0;
+  // const marginLeft = parseFloat(style.marginLeft) || 0;
+  let top = 0; //-marginTop;
+  let left = 0; // -marginLeft;
   {
     let offsetParent:Element|null = e.offsetParent;
     while(e && e !== document && !(e instanceof DocumentFragment)) {
       if(e === offsetParent) {
         break;
       }
-      const style = window.getComputedStyle(e);
-      top += e.offsetTop 
-          - parseFloat(style.marginTop) || 0
-          - e.scrollTop || 0;
-      left += e.offsetLeft
-          - parseFloat(style.marginLeft) || 0
-          - e.scrollLeft || 0;
+      // not accounting for margins here
+      // since in case of margin: auto, offset
+      // may actually be the auto margin
+      // const style = window.getComputedStyle(e);
+      top += e.offsetTop - (e.scrollTop || 0);
+      left += e.offsetLeft - (e.scrollLeft || 0);
       e = e.parentNode || (e as any).host;
     } 
   }
   return {
     left,
     top,
-    rect
+    width: rect.width,
+    height: rect.height
   }
 }
 
 /**
- * 
- * @param e 
- * @param ext 
+ * applies left,top,width and height form ext
+ * also sets marginLeft+Top to 0
+ * @param e HTMLElement to apply extents to
+ * @param ext extents object
  */
 export function applyExtents(e:HTMLElement, ext:any) {
-  e.style.left = ext.left+'px';
-  e.style.top = ext.top+'px';
-  e.style.width = (ext.rect.width)+'px';
-  e.style.height = (ext.rect.height)+'px';
+  e.style.marginLeft = '0px';
+  e.style.marginTop = '0px';
+  e.style.left = (ext.left) + 'px';
+  e.style.top = (ext.top) + 'px';
+  e.style.width = (ext.width) + 'px';
+  e.style.height = (ext.height) + 'px';
 }
 
 
