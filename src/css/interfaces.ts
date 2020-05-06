74eb5330d15cd882f5bce902f9710ce22c3894dd
JanK <sijakret@gmail.com>
2020-05-06 23:19:52 +0200

    feat: better geometry lock

--- src/css/interfaces.ts
+++ src/css/interfaces.ts
@@ -13,13 +13,26 @@ export enum TransitionMode {
   Both = "both"
 }
 
+/**
+ * in-out: enter transition starts playing right away
+ * out-in: enter transition only plays after leave completed
+ * both: both transitions play immediately
+ */
+export enum GeometryLockMode {
+  None = 0,
+  Lock = 1,
+  Auto = 'auto',
+}
+
 export interface CSSClassSequence {
   // class applied throughout whole transition
   active?: string
   // class with initial props applied on first frame
   from?: string
   // class with target props applied after first frame
-  to?: string
+  to?: string,
+  // lock behavior
+  lock?: GeometryLockMode
 }
 
 export interface CSSTransitionOptions {
